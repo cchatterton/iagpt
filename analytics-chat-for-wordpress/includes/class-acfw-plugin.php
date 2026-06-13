681d1437748d1d736c7c1a7db803e866b830d596
TECHN <chris@techn.com.au>
2026-06-13 11:26:27 +1000

    Add plugin bootstrap class

---
+++ analytics-chat-for-wordpress/includes/class-acfw-plugin.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin bootstrap.
+ *
+ * @package AnalyticsChatForWordPress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+final class ACFW_Plugin {
+	private static ?ACFW_Plugin $instance = null;
+
+	private ACFW_Auth $auth;
+	private ACFW_Independent_Analytics $analytics;
+	private ACFW_REST $rest;
+	private ACFW_Settings $settings;
+
+	private function __construct() {
+		$this->auth      = new ACFW_Auth();
+		$this->analytics = new ACFW_Independent_Analytics();
+		$this->rest      = new ACFW_REST( $this->auth, $this->analytics );
+		$this->settings  = new ACFW_Settings( $this->auth, $this->analytics );
+	}
+
+	public static function instance(): ACFW_Plugin {
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	public function init(): void {
+		add_filter( 'rest_pre_dispatch', array( $this->rest, 'authenticate_pre_dispatch' ), 10, 3 );
+		add_action( 'rest_api_init', array( $this->rest, 'register_routes' ) );
+		add_action( 'admin_menu', array( $this->settings, 'register_admin_page' ) );
+		add_action( 'admin_init', array( $this->settings, 'register_settings' ) );
+	}
+}
